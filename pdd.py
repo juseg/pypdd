0fc018d0828930b454c91851fbd4e642baa4fae2
Julien Seguinot <julien.seguinot@natgeo.su.se>
2013-02-28 11:01:13 +0100

    Created a PDDModel class
    Now a pdd model need to be instanted with pdd=PDDModel() and all computations are accessed by pdd.method().

--- pdd.py
+++ pdd.py
@@ -3,34 +3,51 @@
 import numpy as np
 from netCDF4 import Dataset as NC
 
-# PDD model parameters
+# PDD model class
 
-temp_snow = 0
-temp_rain = 2
-pdd_factor_snow = 0.003
-pdd_factor_ice = 0.008
-pdd_refreeze = 0.6
-pdd_std_dev = 5
+class PDDModel():
+	"""A positive degree-day model for glacier surface mass balance"""
 
-# PDD model computations
+	def __init__(self,
+		pdd_factor_snow = 0.003,
+		pdd_factor_ice  = 0.008,
+		pdd_refreeze    = 0.6,
+		pdd_std_dev     = 5,
+		temp_snow       = 0,
+		temp_rain       = 2):
+		"""Initiate PDD model with given parameters"""
+		
+		# set pdd model parameters
+		self.pdd_factor_snow = pdd_factor_snow
+		self.pdd_factor_ice  = pdd_factor_ice
+		self.pdd_refreeze    = pdd_refreeze
+		self.pdd_std_dev     = pdd_std_dev
+		self.temp_snow       = temp_snow
+		self.temp_rain       = temp_rain
 
-def pdd(temp):
+	def pdd(self, temp):
 		"""Compute positive degree days from temperature time series"""
 		return sum(np.greater(temp,0)*temp)*365.242198781/12
 
-def snowfrac(temp):
+	def snowfrac(self, temp):
 		"""Compute snow fraction from temperature"""
-		return np.clip((temp_rain-temp)/(temp_rain-temp_snow), 0, 1)
+		reduced_temp = (self.temp_rain-temp) / (self.temp_rain-self.temp_snow)
+		return np.clip(reduced_temp, 0, 1)
 
-def snow(temp, prec):
+	def snow(self, temp, prec):
 		"""Compute snow fall from temperature and precipitation"""
-		return sum(snowfrac(temp)*prec)
+		return sum(self.snowfrac(temp)*prec)
 
-def smb(snow, pdd):
-		"""Compute surface mass balance from snow fall and pdd sum"""
-		return np.where(pdd_factor_snow*pdd < snow,
-			snow - pdd_factor_snow*pdd,
-			pdd_refreeze*snow - pdd_factor_ice*(pdd-snow/pdd_factor_snow))
+	def smb(self, snow, pdd):
+		"""Compute surface mass balance from snow precipitation and pdd sum"""
+
+		# compute snow accumulation after melt
+		snow_acc = snow - pdd * self.pdd_factor_snow
+
+		# if positive, return accumulated snow
+		# if negative, compute refreezing and ice melt
+		return np.where(snow_acc > 0, snow_acc, snow*self.pdd_refreeze
+			+ snow_acc*self.pdd_factor_ice/self.pdd_factor_snow)
 
 # netCDF IO
 
@@ -82,20 +99,22 @@ def main():
 
 		# create dimensions
 		for dimname, dim in i.dimensions.items():
-			print dimname, len(dim)
 			o.createDimension(dimname, len(dim))
+
+		# initiate PDD model
+		pdd=PDDModel()
 
 		# compute the number of positive degree days
 		pddvar = o.createVariable('pdd', 'f4', ('x', 'y'))
-		pddvar[:] = pdd(temp)
+		pddvar[:] = pdd.pdd(temp)
 
 		# compute snowfall
 		snowvar = o.createVariable('snow', 'f4', ('x', 'y'))
-		snowvar[:] = snow(temp, prec)
+		snowvar[:] = pdd.snow(temp, prec)
 
 		# compute surface mass balance
 		smbvar = o.createVariable('smb', 'f4', ('x', 'y'))
-		smbvar[:] = smb(snowvar[:], pddvar[:])
+		smbvar[:] = pdd.smb(snowvar[:], pddvar[:])
 
 # Called at execution
 
