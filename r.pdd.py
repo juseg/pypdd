70a91e4bf6deb5d99b97c5b7c7666fea4c7b308e
Julien Seguinot <julien.seguinot@natgeo.su.se>
2014-04-06 12:17:50 +0200

    Update outdated GRASS script
    * Comply to PEP 8 / GRASS style guide
    * Output all cumulative variables

--- r.pdd.py
+++ r.pdd.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python
 
-############################################################################
-#
-# MODULE:      r.in.pdd
-#
-# AUTHOR(S):   Julien Seguinot
-#
-# PURPOSE:     Positive Degree Day (PDD) model for glacier mass balance
-#
-# COPYRIGHT:   (c) 2013 Julien Seguinot
-#
-#     This program is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
-# 
-#     This program is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
-# 
-#     You should have received a copy of the GNU General Public License
-#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-############################################################################
+"""
+MODULE:      r.in.pdd
+
+AUTHOR(S):   Julien Seguinot
+
+PURPOSE:     Positive Degree Day (PDD) model for glacier mass balance
+
+COPYRIGHT:   (c) 2013-2014 Julien Seguinot
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 #%Module
 #% description: Positive Degree Day (PDD) model for glacier mass balance
@@ -46,6 +44,7 @@
 #% required: yes
 #% multiple: yes
 #%end
+
 #%option
 #% key: pdd
 #% type: string
@@ -54,38 +53,53 @@
 #% required: no
 #%end
 #%option
-#% key: snow
+#% key: accu
 #% type: string
 #% gisprompt: new,cell,raster
-#% description: Name for output snow precipitation map
+#% description: Name for output ice-equivalent surface accumulation map
+#% required: no
+#%end
+#%option
+#% key: snow_melt
+#% type: string
+#% gisprompt: new,cell,raster
+#% description: Name for output ice-equivalent surface melt of snow map
+#% required: no
+#%end
+#%option
+#% key: ice_melt
+#% type: string
+#% gisprompt: new,cell,raster
+#% description: Name for output ice-equivalent surface melt of ice map
 #% required: no
 #%end
 #%option
 #% key: melt
 #% type: string
 #% gisprompt: new,cell,raster
-#% description: Name for output surface melt map
+#% description: Name for output ice-equivalent surface melt map
 #% required: no
 #%end
 #%option
 #% key: runoff
 #% type: string
 #% gisprompt: new,cell,raster
-#% description: Name for output surface runoff map
+#% description: Name for output ice-equivalent surface meltwater runoff map
 #% required: no
 #%end
 #%option
 #% key: smb
 #% type: string
 #% gisprompt: new,cell,raster
-#% description: Name for output surface mass balance raster map
+#% description: Name for output ice-equivalent surface mass balance map
 #% required: no
 #%end
 
-import numpy as np                    # scientific module Numpy [1]
 import grass.script as grass
-import grass.script.array as garray
-from pypdd import PDDModel
+from grass.script import array
+import numpy as np         # scientific module Numpy [1]
+from pypdd import PDDModel # positive degree day model PyPDD [2]
+
 
 ### Main function ###
 
@@ -95,39 +109,35 @@ def main():
     # parse arguments
     temp_maps = options['temp'].split(',')
     prec_maps = options['prec'].split(',')
-    pdd_map   = options['pdd']
-    snow_map  = options['snow']
-    melt_map  = options['melt']
-    runoff_map= options['runoff']
-    smb_map   = options['smb']
 
     # read temperature maps
     grass.info('reading temperature maps...')
     temp = [grass.array.array()] * 12
     for i, m in enumerate(temp_maps):
-      temp[i].read(m)
-      grass.percent(i, 12, 1)
+        temp[i].read(m)
+        grass.percent(i, 12, 1)
 
     # read precipitation maps
     grass.info('reading precipitation maps...')
     prec = [grass.array.array()] * 12
     for i, m in enumerate(prec_maps):
-      prec[i].read(m)
-      grass.percent(i, 12, 1)
+        prec[i].read(m)
+        grass.percent(i, 12, 1)
 
     # run PDD model
     grass.info('running PDD model...')
     temp = np.array(temp)
     prec = np.array(prec)
     pdd = PDDModel()
-    smb = pdd(temp, prec, big=True)
+    smb = pdd(temp, prec)
 
     # write output maps
     grass.info('writing output maps...')
-    for key, value in smb.iteritems():
-      if options[key]:
-        a = garray.array()
-        a[:] = value
+    for varname in ['pdd', 'accu', 'snow_melt', 'ice_melt', 'melt',
+                    'runoff', 'smb']:
+        if options[varname]:
+            a = grass.array.array()
+            a[:] = smb[varname]
 
 ### Main program ###
 
@@ -137,4 +147,5 @@ if __name__ == "__main__":
 
 # Links
 # [1] http://numpy.scipy.org
+# [2] http://github.com/jsegu/pypdd
 
